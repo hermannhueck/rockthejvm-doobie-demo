7dd780e0bc3911e75a12edffbe684c08af2bc007
Hermann Hueck <hermann.hueck@mailbox.org>
2022-04-08 18:16:51 +0200

    build: bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,9 +10,12 @@ object Versions {
   val doobieVersion     = "1.0.0-RC2"
   val newTypeVersion    = "0.4.4"
   val munitVersion      = "0.7.29"
-  val scalaCheckVersion = "1.15.4"
+  val scalaCheckVersion = "1.16.0"
 
   // compiler plugins
   val kindProjectorVersion    = "0.13.2"
   val betterMonadicForVersion = "0.3.1"
+
+  // scalafix plugins
+  val scalafixOrganizeImportsVersion = "0.6.0"
 }
